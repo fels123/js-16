e09fd19df00f0f955ae4885ad918d17006e4283d
fels123 <kelengulienakhro@gmail.com>
2024-06-16 21:07:01 +0530

    excercise complete

--- excercise.js
+++ excercise.js
@@ -1,11 +1,10 @@
-// 1. To be precise, you must add two "course goal"
-//  items => two <li> items to the <ul> that already
-//   exists on the page:
-
-// 2. The first course goal <li>
-// that's added via JavaScript should contain
-// the text "Learn JavaScript" (without quotes)
-
-// 3. The second goal <li> item should contain
-// the text "Master JavaScript"
-//  (also without quotes)
+function solve() {
+  // Your solution goes here
+  const goals = document.querySelector("ul");
+  const li1 = document.createElement("li");
+  li1.textContent = "Learn JavaScript";
+  const li2 = document.createElement("li");
+  li2.textContent = "Master JavaScript";
+  goals.appendChild(li1);
+  goals.appendChild(li2);
+}
